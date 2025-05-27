79a69d0316a8de1bebbc14c5741ce42ff77eb698
reedjaz <reedjaz.reedjaz@gmail.com>
2025-05-27 17:29:27 +0700

    Tes fullscreen

--- js/sw.js
+++ js/sw.js
@@ -1,3 +1,14 @@
 self.addEventListener('fetch', function(event) {
     // Default caching behavior
 });
+
+function goFullscreen() {
+    const elem = document.documentElement; // bisa diganti ke canvas juga
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { // Safari
+      elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { // IE/Edge lama
+      elem.msRequestFullscreen();
+    }
+  }
